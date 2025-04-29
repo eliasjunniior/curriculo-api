2239d2414611c6b7bf8ef9f986ca9691e5d0e589
eliasjunniior <93531081+eliasjunniior@users.noreply.github.com>
2025-04-29 17:36:22 +0000

    Ajustes

--- curriculo-api/src/main/java/com/eliasjunnior/curriculo_api/CurriculoApiApplication.java
+++ curriculo-api/src/main/java/com/eliasjunnior/curriculo_api/CurriculoApiApplication.java
@@ -1,9 +1,9 @@
-package com.eliasjunnior.curriculo_api;
+package com.eliasjunnior.curriculoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class CurriculoApiApplication {
 
 	public static void main(String[] args) {
